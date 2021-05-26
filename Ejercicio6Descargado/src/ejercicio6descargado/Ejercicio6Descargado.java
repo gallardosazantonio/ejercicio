39f41c82d5a4b6ff0bb83cafac911e1a623d61de
gallardosazantonio <gallardosazantonio@gmail.com>
2021-05-26 20:01:31 +0200

    Version 2.0

---
+++ Ejercicio6Descargado/src/ejercicio6descargado/Ejercicio6Descargado.java
@@ -0,0 +1,10 @@
+
+package ejercicio6descargado;
+
+public class Ejercicio6Descargado {
+
+    public static void main(String[] args) {
+        System.out.println("Hola mundo que tal estas");
+    }
+    
+}

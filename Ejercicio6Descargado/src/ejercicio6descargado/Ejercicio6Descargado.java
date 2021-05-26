e6eb58b05ed23f9a6cfbe32c9601098a75d65c61
gallardosazantonio <gallardosazantonio@gmail.com>
2021-05-26 20:06:18 +0200

    Version 4.0

--- Ejercicio6Descargado/src/ejercicio6descargado/Ejercicio6Descargado.java
+++ Ejercicio6Descargado/src/ejercicio6descargado/Ejercicio6Descargado.java
@@ -1,12 +1,25 @@
 
 package ejercicio6descargado;
 
+import java.util.Scanner;
+
 public class Ejercicio6Descargado {
 
     public static void main(String[] args) {
         System.out.println("Hola mundo que tal estas");
         
         System.out.println("Dime en que dia estas");
+        
+        
+        Scanner teclado =new Scanner(System.in);
+        
+        int numero=teclado.nextInt();
+        
+        
+        System.out.println(numero);
+        
+        
     }
+    
     
 }
